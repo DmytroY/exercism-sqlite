eab5ca94279e0bdf16904b9fdba7f38bf901e355
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-08-08 08:23:51 +0200

    dif_of_sqr2

--- difference-of-squares/difference-of-squares.sql
+++ difference-of-squares/difference-of-squares.sql
@@ -1,11 +1,33 @@
 -- Schema: CREATE TABLE "difference-of-squares" ("number" INT, "property" TEXT, "result" INT);
 -- Task: update the difference-of-squares table and set the result based on the number and property fields.
 
-UPDATE "difference-of-squares" SET result =(
-    CASE "property"
-        WHEN 'squareOfSum' THEN pow((1 + number)*number/2, 2)
-        WHEN 'sumOfSquares' THEN  (number * (number + 1) * (2 * number + 1) / 6)
-        WHEN 'differenceOfSquares' THEN  pow((1 + number)*number/2, 2) - (number * (number + 1) * (2 * number + 1) / 6)
-        ELSE 0
-    END
-);
+UPDATE "difference-of-squares"
+    SET result = (
+        WITH RECURSIVE series(n) AS (
+            SELECT 1
+            UNION ALL
+            SELECT n + 1 FROM series WHERE n < "difference-of-squares".number
+        ) SELECT pow(sum(n), 2) FROM series
+    )
+    WHERE "property" == 'squareOfSum';
+
+UPDATE "difference-of-squares"
+    SET result =(
+        WITH RECURSIVE series(n) AS (
+            SELECT 1
+            UNION ALL
+            SELECT n+1 FROM series WHERE n < "difference-of-squares".number
+        ) SELECT sum(pow(n, 2)) FROM series
+    )
+    WHERE "property" == 'sumOfSquares';
+
+UPDATE "difference-of-squares"
+    SET result = (
+        WITH RECURSIVE series(n) AS (
+            SELECT 1
+            UNION ALL
+            SELECT n+1 FROM series WHERE n < "difference-of-squares".number
+        ) SELECT pow(sum(n), 2) - sum(pow(n, 2)) FROM series
+    )
+    WHERE "property" == 'differenceOfSquares';
+
