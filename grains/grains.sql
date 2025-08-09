353cbed51fc61fecae4241c431b23727cba92e6f
Dmytro <dmitry.yakovenko@gmail.com>
2025-08-09 03:46:06 +0200

    Update grains.sql

--- grains/grains.sql
+++ grains/grains.sql
@@ -1,14 +1,6 @@
 -- Schema: CREATE TABLE "grains" ("task" TEXT, "square" INT, "result" INT);
 -- Task: update the grains table and set the result based on the task (and square fields).
 
--- UPDATE grains SET result = IIF(task == 'single-square', pow(2, square-1),
---         WITH RECURSIVE series(n) AS(
---             SELECT 1
---             UNION ALL
---             SELECT n * 2 WHERE n < 64
---         ) SELECT sum(n) FROM series
---     );
-
 UPDATE grains SET result = pow(2, square-1) WHERE task == 'single-square';
 UPDATE grains SET result =(
         WITH RECURSIVE series(n, grains) AS (
