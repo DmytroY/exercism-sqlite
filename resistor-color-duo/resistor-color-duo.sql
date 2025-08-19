5cbf308e2bc791acbe017e6d9f25676a0d3f1040
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-08-19 16:03:58 +0200

    color-duo

---
+++ resistor-color-duo/resistor-color-duo.sql
@@ -0,0 +1,20 @@
+-- Schema: CREATE TABLE "color_code" ("color1" TEXT, "color2" TEXT, "result" INT);
+-- Task: update the color_code table and set the result based on the two colors.
+
+CREATE TEMP TABLE "colors" ("color" TEXT, "digit" INT);
+INSERT INTO "colors"(color, digit) VALUES
+    ('black', 0),
+    ('brown', 1),
+    ('red', 2),
+    ('orange', 3),
+    ('yellow', 4),
+    ('green', 5),
+    ('blue', 6),
+    ('violet', 7),
+    ('grey', 8),
+    ('white', 9);
+
+UPDATE "color_code" SET result = (
+    (SELECT digit FROM colors WHERE "color_code".color1 = colors.color) * 10 +
+    (SELECT digit FROM colors WHERE "color_code".color2 = colors.color)
+);
