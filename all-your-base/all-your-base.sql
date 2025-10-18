fb95264ea3cb40bb5e1584f672e6818a4598211f
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-10-18 13:01:55 +0200

    alyourbase

--- all-your-base/all-your-base.sql
+++ all-your-base/all-your-base.sql
@@ -10,7 +10,6 @@
 --       * the result column should contain JSON-encoded data: an
 --         object with the digits as integers or a descripition of any errors.
 
--- process error and edge cases, convert to decimal
 UPDATE "all-your-base" SET result = (
     CASE
         WHEN input_base < 2 THEN json('{"error":"input base must be >= 2"}')
@@ -19,25 +18,25 @@ UPDATE "all-your-base" SET result = (
             OR
             (SELECT MAX(value) FROM json_each(digits)) >= input_base
             THEN json('{"error":"all digits must satisfy 0 <= d < input base"}')
-        WHEN digits = '[]' THEN 0
+        WHEN digits = '[]' THEN '{"digits":[0]}'
         ELSE (
-            WITH RECURSIVE series(n, v) AS  (
-                SELECT 0, (digits ->> (json_array_length(digits) - 1)) * pow(input_base, 0)
-                UNION ALL
-                SELECT n+1, (digits ->> (json_array_length(digits) - 2 - n)) * pow(input_base, n+1) FROM series WHERE (n+1) < json_array_length(digits)
-            ) SELECT CAST(SUM(v) AS INTEGER) FROM series
+            WITH inbase(i, s) AS (
+                    SELECT 0, (digits ->> 0) * pow(input_base, json_array_length(digits) - 1 - 0)
+                    UNION ALL
+                    SELECT i + 1, s + (digits ->> (i + 1)) * pow(input_base, json_array_length(digits) - 1 -(i + 1))
+                    FROM inbase WHERE i < json_array_length(digits) - 1
+                ),
+                outbase(j, d, b) AS(
+                    SELECT 0,
+                        CAST((SELECT s FROM inbase WHERE i = json_array_length(digits) - 1) /output_base AS INTEGER),
+                        CAST((SELECT s FROM inbase WHERE i = json_array_length(digits) - 1) % output_base AS INTEGER)
+                    UNION ALL
+                    SELECT j + 1,
+                        CAST(d / output_base AS INTEGER),
+                        CAST(d % output_base AS INTEGER)
+                    FROM outbase WHERE d > 0
+                )
+            SELECT json_object('digits', json_group_array(b)) FROM (SELECT b FROM outbase ORDER BY j DESC)
         )
     END
 );
-
--- converd decimal to result
-UPDATE "all-your-base" SET result =(
-    SELECT json_insert('{}', '$.digits', json_group_array(v))
-    FROM(
-        WITH RECURSIVE series (n, q, v) AS(
-            SELECT 0, CAST(result / output_base AS INTEGER) , CAST(result % output_base AS INTEGER)
-            UNION ALL
-            SELECT n + 1, CAST(q / output_base AS INTEGER), cast(q % output_base AS INTEGER) FROM series WHERE q >= 1
-        ) SELECT v FROM series ORDER BY n DESC
-    ) 
-)WHERE result GLOB('[0-9.]*');
